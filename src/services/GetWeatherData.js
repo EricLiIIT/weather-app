a899f91ec4ea01b7766c5e156cae7054096aa06e
Eric <eli7@hawk.iit.edu>
2022-09-24 17:19:26 -0500

    fix: include extra data on weather api call

--- src/services/GetWeatherData.js
+++ src/services/GetWeatherData.js
@@ -1,5 +1,5 @@
 export async function getWeatherData(latitude, longitude) {
-  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude.toString()}&longitude=${longitude.toString()}&daily=weathercode,temperature_2m_max,temperature_2m_min&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`;
+  const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude.toString()}&longitude=${longitude.toString()}&daily=weathercode,temperature_2m_max,temperature_2m_min,sunrise,sunset,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timezone=auto`;
   const response = await fetch(url);
   const data = await response.json();
 
