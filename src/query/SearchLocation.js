1e32023b16dcddef84635e718280517bb06cbd3f
Eric <eli7@hawk.iit.edu>
2022-09-24 17:17:59 -0500

    style: center form inputs

--- src/query/SearchLocation.js
+++ src/query/SearchLocation.js
@@ -1,5 +1,6 @@
 import { React, useState, useEffect } from "react";
 import { getCityCoordinates } from "../services/GetCity";
+import "./SearchLocation.css";
 import Card from "../weather/Card.js";
 
 export default function SearchLocation() {
