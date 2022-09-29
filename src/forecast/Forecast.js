c0e01fd986cf6a67aca119098fcef989affdfdd7
Eric <eli7@hawk.iit.edu>
2022-09-29 00:57:13 -0500

    style: add hover animation on forecast selection

--- src/forecast/Forecast.js
+++ src/forecast/Forecast.js
@@ -1,20 +1,9 @@
-import Card from "../weather/Card";
 import { Day } from "./Day";
 import "./Forecast.css";
 const Forecast = (props) => {
   return (
     <div className="forecast">
       <Day day={props.day} maxTemp={props.maxTemp} minTemp={props.minTemp} />
-      {/* <Card
-        day={i}
-        weather={props.weather}
-        maxTemp={props.maxTemp[index]}
-        minTemp={props.minTemp[index]}
-        sunrise={props.sunrise[index]}
-        sunset={props.sunset[index]}
-        precipitation={props.precipitation[index]}
-        weatherCode={props.weatherCode[index]}
-      /> */}
     </div>
   );
 };
